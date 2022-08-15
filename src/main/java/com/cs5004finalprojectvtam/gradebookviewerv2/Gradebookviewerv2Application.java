ab9d3008e55b872e4156617b1c9274b5816b5369
Vincent Tam <vtam2012@gmail.com>
2022-08-15 15:49:29 -0700

    initialize repo

---
+++ src/main/java/com/cs5004finalprojectvtam/gradebookviewerv2/Gradebookviewerv2Application.java
@@ -0,0 +1,13 @@
+package com.cs5004finalprojectvtam.gradebookviewerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradebookviewerv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gradebookviewerv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs5004finalprojectvtam/gradebookviewerv2/Gradebookviewerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs5004finalprojectvtam.gradebookviewerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradebookviewerv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
